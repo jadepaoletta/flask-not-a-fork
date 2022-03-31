c4165aecd3c10f7768fb53ddc2f828bf9d3c35c1
Jade Paoletta <jadepaoletta27@gmail.com>
2022-03-31 14:45:55 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return redirect('success')
+
+@app.route('/success')
+def success():
+    return 'this is so successful, blah blah test test'
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=4000, debug=True)
